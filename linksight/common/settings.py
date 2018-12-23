befe3f1d629e16d2bedc1e27c232630b959366bc
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-23 16:59:39 +0800

    Add common package

---
+++ linksight/common/settings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+VERSION = '0.1'
+USER_AGENT = 'linksight-api-client/{}'.format(VERSION)
+ENDPOINT = 'https://linksight-stg.thinkingmachin.es/api'
---
+++ linksight/common/utils.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def urljoin(*args):
+    return '/'.join(map(lambda a: a.strip('/'), args))
