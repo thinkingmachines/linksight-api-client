964a58c4538e5196acfd85accdf9bf311288184c
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-23 19:39:53 +0800

    Add Client to auto-imports in __init__

--- linksight/__init__.py
+++ linksight/__init__.py
@@ -2,6 +2,12 @@
 
 """LinkSight Python API Client"""
 
+from .client import Client
+
+# Import client right away
+__all__ = ['Client']
+
 __version__ = '0.1'
 __author__ = 'Thinking Machines Data Science'
 __email__ = 'hello@thinkingmachin.es'
+
