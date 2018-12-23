aa2c3607b65faa59882efa60ad73c37bdb5e1feb
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-23 13:38:45 +0800

    Update setup.py for verbosity

--- setup.py
+++ setup.py
@@ -1,9 +1,42 @@
-from setuptools import setup
+# -*- coding: utf-8 -*-
 
-if __name__ == '__main__':
-    setup(
-        name='linksight-api-client',
-        version='0.1',
-        py_modules=['linksight'],
-        install_requires=['requests'],
-    )
+# Import modules
+from setuptools import find_packages, setup
+
+with open('README.md', encoding='utf8') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('requirements-dev.txt') as f:
+    dev_requirements = f.read().splitlines()
+
+setup(
+    name='linksight-api-client',
+    version='1.0.0',
+    description='Python API client for LinkSight',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Thinking Machines Data Science',
+    author_email='hello@thinkingmachin.es',
+    url='https://github.com/thinkingmachines/linksight-api-client',
+    packages=find_packages(exclude=['docs', 'tests']),
+    include_package_data=True,
+    install_requires=requirements,
+    tests_require=dev_requirements,
+    extras_require={'test': dev_requirements},
+    license='GNU General Public License v2 (GPLv2)',
+    zip_safe=False,
+    keywords=['linksight', 'python client', 'geospatial'],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
