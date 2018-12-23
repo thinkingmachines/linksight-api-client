7d523705a520a738e706a74dbc20492ef866062c
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-23 16:59:48 +0800

    Add resource package

---
+++ linksight/resource/base.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Import standard library
+import logging
+
+# Import modules
+import coloredlogs
+
+from ..common.settings import ENDPOINT
+from ..common.utils import urljoin
+
+
+class Resource(dict):
+    """Base Resource class"""
+
+    @classmethod
+    def create(cls, client, **kwargs):
+        """Create a POST request to LinkSight endpoint
+
+        Parameters
+        ----------
+        client : requests.Session
+            An instance of the client
+
+        Returns
+        -------
+        dict
+            An instance of itself with the appropriate response
+        """
+        url = urljoin(ENDPOINT, cls.url)
+        resp = client.request('post', url, **kwargs)
+        resp.raise_for_status()
+        return cls(client, resp)
+
+    @classmethod
+    def retrieve(cls, client, id):
+        """Create a GET request to LinkSight endpoint
+
+        Parameters
+        ----------
+        client : requests.Session
+            An instance of the client
+        id : string
+            ID of the user
+
+        Returns
+        -------
+        dict
+            An instance of itself with the appropriate response
+        """
+        url = urljoin(ENDPOINT, cls.url, id)
+        resp = client.request('get', url)
+        resp.raise_for_status()
+        return cls(client, resp)
+
+    def __init__(self, client, resp):
+        super().__init__(resp.json())
+        self.client = client
+        self.response = resp
+        # Create a logger
+        self.logger = logging.getLogger(__name__)
+        coloredlogs.install(level=logging.INFO, logger=self.logger)
+
+    def create_instance_url(self, *args):
+        """Create the instance url to pass the request onto
+
+        Parameters
+        ----------
+        args : strings
+
+        Returns
+        -------
+        string
+            The instance url
+        """
+        return urljoin(ENDPOINT, self.url, self['id'], *args)
---
+++ linksight/resource/resources.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+"""Contains various resources for API calls"""
+
+# Import standard library
+import logging
+
+# Import modules
+import coloredlogs
+
+from ..resource.base import Resource
+
+
+class User(Resource):
+    """Resource for handling user-related API calls"""
+
+    url = 'users'
+
+
+class Match(Resource):
+    """Resource for handling resulting dataset matches"""
+
+    url = 'matches'
+
+
+class Dataset(Resource):
+    """Resource for handling dataset-related API calls"""
+
+    url = 'datasets'
+
+    def match(
+        self,
+        source_bgy_col=None,
+        source_municity_col=None,
+        source_prov_col=None,
+        export=True,
+    ):
+        """Perform a match using the LinkSight API
+
+        Parameters
+        ----------
+        source_bgy_col : string
+            Column name for the barangays
+        source_municity_col : string
+            Column name for the municipality
+        source_prov_col : string
+            Column name for the province
+        export : bool
+            Export the resulting match (default is True)
+
+        Returns
+        -------
+        dict
+            The API response containing the match
+        """
+        if not any([source_bgy_col, source_municity_col, source_prov_col]):
+            msg = 'At least one column must be defined!'
+            self.logger.error(msg)
+            raise ValueError(msg)
+        url = self.create_instance_url('match')
+        json_request = {
+            'source_bgy_col': source_bgy_col,
+            'source_municity_col': source_municity_col,
+            'source_prov_col': source_prov_col,
+            'export': export,
+        }
+        resp = self.client.request('post', url, json=json_request)
+        return Match(self.client, resp)
