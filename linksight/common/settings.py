60c97d859b651bb93aee02194446d6f6d28a4b69
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-23 20:22:49 +0800

    Add docstrings for common.settings

--- linksight/common/settings.py
+++ linksight/common/settings.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+"""Global-wide constants for package settings"""
+
 VERSION = '0.1'
 USER_AGENT = 'linksight-api-client/{}'.format(VERSION)
 ENDPOINT = 'https://linksight-stg.thinkingmachin.es/api'
