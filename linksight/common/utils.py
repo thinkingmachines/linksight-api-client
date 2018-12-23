724d7aa587f5e799280f4319bfeb46e66641daaa
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-23 20:20:54 +0800

    Add docstrings for common.utils

--- linksight/common/utils.py
+++ linksight/common/utils.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+"""Utility methods common to the package"""
+
 
 def urljoin(*args):
+    """Join a set of strings for URL creation"""
     return '/'.join(map(lambda a: a.strip('/'), args))
