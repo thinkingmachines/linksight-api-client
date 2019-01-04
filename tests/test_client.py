3fe24e9c6fd59480ab3fd7dc6e519326fc36ef0e
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-01-04 11:54:30 +0800

    Add tests for df validation

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,6 +7,7 @@ import glob
 
 # Import modules
 import pytest
+import pandas as pd
 
 # Import from package
 import linksight as ls
@@ -28,6 +29,15 @@ def test_client_create_dataset(mock_api, client, dataset_response):
     assert resp.keys() == dataset_response.keys()
 
 
+def test_client_create_dataset_from_df(mock_api, client, dataset_response):
+    """Test if Client.create_dataset() works given a Dataframe input"""
+    file = glob.glob('./**/test_areas.csv', recursive=True)
+    df = pd.read_csv(file[0])
+    resp = client.create_dataset(df)
+    assert isinstance(resp, ls.resource.resources.Dataset)
+    assert resp.keys() == dataset_response.keys()
+
+
 @pytest.mark.webtest
 def test_client_get_user_web(client):
     """Test if Client.get_user() returns the expected type"""
@@ -41,4 +51,13 @@ def test_client_create_dataset_web(client):
     file = glob.glob('./**/test_areas.csv', recursive=True)
     with open(file[0], 'r') as fp:
         resp = client.create_dataset(fp)
+    assert isinstance(resp, ls.resource.resources.Dataset)
+
+
+@pytest.mark.webtest
+def test_client_create_dataset_from_df_web(client):
+    """Test if Client.create_dataset() works given a Dataframe input"""
+    file = glob.glob('./**/test_areas.csv', recursive=True)
+    df = pd.read_csv(file[0])
+    resp = client.create_dataset(df)
     assert isinstance(resp, ls.resource.resources.Dataset)
