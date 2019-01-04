3afa3681f14df7b82b6729956dda8617245db1eb
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-01-04 19:00:05 +0800

    Add tests for catching no column names

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -22,6 +22,15 @@ def test_dataset_match(mock_api, client, match_response):
     assert resp.keys() == match_response.keys()
 
 
+def test_dataset_match_no_columns(mock_api, client):
+    """Test if ValueError is raised whenever no column names are passed"""
+    file = glob.glob('./**/test_areas.csv', recursive=True)
+    with open(file[0], 'r') as fp:
+        ds = client.create_dataset(fp)
+    with pytest.raises(ValueError):
+        ds.match()
+
+
 def test_dataset_match_invalid_columns(mock_api_server_error, client):
     """Test if ValueError is raised given invalid column names (500)"""
     file = glob.glob('./**/test_areas.csv', recursive=True)
@@ -42,7 +51,17 @@ def test_dataset_match_web(client):
 
 
 @pytest.mark.webtest
-def test_dataset_match_invalid_columns(client):
+def test_dataset_match_no_columns_web(client):
+    """Test if ValueError is raised whenever no column names are passed"""
+    file = glob.glob('./**/test_areas.csv', recursive=True)
+    with open(file[0], 'r') as fp:
+        ds = client.create_dataset(fp)
+    with pytest.raises(ValueError):
+        ds.match()  # No column names
+
+
+@pytest.mark.webtest
+def test_dataset_match_invalid_columns_web(client):
     """Test if ValueError is raised given invalid column names (500)"""
     file = glob.glob('./**/test_areas.csv', recursive=True)
     with open(file[0], 'r') as fp:
